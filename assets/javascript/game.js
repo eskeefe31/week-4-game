b0472b1b3ffdcced66a89430b282ed067ca0605f
Bess Keefe <elizabeth.keefe@clt-m002.home>
2018-01-10 23:06:51 -0500

    index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var numberChoices = [1,2,3,4,5,6,7,8,9,10]
+
+var wins = 0;
+var losses = 0;
+var crystal1 = numberChoices [Math.floor(Math.random()*numberChoices.length)];
+var crystal2 = numberChoices [Math.floor(Math.random()*numberChoices.length)];
+var Crystal3 = numberChoices [Math.floor(Math.random()*numberChoices.length)];
+var Crystal4 = numberChoices [Math.floor(Math.random()*numberChoices.length)];
+
+
